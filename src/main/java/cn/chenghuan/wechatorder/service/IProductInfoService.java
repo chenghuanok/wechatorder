5f8976945dedad8da2d3316ef2e249e7ea8b6c2a
chenghuanok <1832398432@qq.com>
2019-06-03 21:54:49 +0800

    商品service

---
+++ src/main/java/cn/chenghuan/wechatorder/service/IProductInfoService.java
@@ -0,0 +1,14 @@
+package cn.chenghuan.wechatorder.service;
+
+/**
+ * @author 程欢
+ * @Description 商品service
+ * @Date 2019/6/3 21:50
+ */
+public interface IProductInfoService {
+
+    /**.
+     * 保存商品
+     */
+    void saveProductInfo();
+}
---
+++ src/main/java/cn/chenghuan/wechatorder/service/impl/ProductInfoService.java
@@ -0,0 +1,21 @@
+package cn.chenghuan.wechatorder.service.impl;
+
+import cn.chenghuan.wechatorder.service.IProductInfoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 程欢
+ * @Description 商品service
+ * @Date 2019/6/3 21:51
+ */
+@Service
+public class ProductInfoService implements IProductInfoService {
+    /**
+     * .
+     * 保存商品
+     */
+    @Override
+    public void saveProductInfo() {
+
+    }
+}
