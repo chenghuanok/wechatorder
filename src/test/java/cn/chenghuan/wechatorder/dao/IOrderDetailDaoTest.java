2c50d015890a7a882dfeabe2fc488c89a2cc993c
chenghuanok <1832398432@qq.com>
2019-06-18 21:36:21 +0800

    订单与订单详情测试用例

--- src/test/java/cn/chenghuan/wechatorder/dao/IOrderDetailDaoTest.java
+++ src/test/java/cn/chenghuan/wechatorder/dao/IOrderDetailDaoTest.java
@@ -1,6 +1,17 @@
 package cn.chenghuan.wechatorder.dao;
 
+import cn.chenghuan.wechatorder.domain.OrderDetail;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
@@ -9,10 +20,32 @@ import static org.junit.Assert.*;
  * @Description 订单详情测试类
  * @Date 2019/6/17 22:09
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class IOrderDetailDaoTest {
+
+    @Autowired
+    private IOrderDetailDao orderDetailDao;
+
+    @Test
+    public void saveOrderDetail(){
+       final OrderDetail orderDetail = new OrderDetail();
+       final Date date = new Date();
+       orderDetail.setGid(UUID.randomUUID().toString().replace("-",""));
+       orderDetail.setOrderId("123");
+       orderDetail.setProductId("456");
+       orderDetail.setProductName("鞋子");
+       orderDetail.setProductPrice(new BigDecimal(2.68));
+       orderDetail.setProductQuantity(100);
+       orderDetail.setProductIcon("123");
+       orderDetail.setCreateTime(date);
+       orderDetail.setUpdateTime(date);
+       orderDetailDao.save(orderDetail);
+    }
 
     @Test
     public void findByOrderId() {
-
+      List<OrderDetail> orderDetailList = orderDetailDao.findByOrderId("123");
+      Assert.assertNotNull(orderDetailList);
     }
 }
