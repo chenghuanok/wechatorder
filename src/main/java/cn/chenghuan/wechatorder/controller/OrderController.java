1b1e9dd80fee9ef3f8e54e75a4a570212a9c2afe
chenghuanok <1832398432@qq.com>
2019-07-10 23:12:40 +0800

    引入fastjson

--- src/main/java/cn/chenghuan/wechatorder/controller/OrderController.java
+++ src/main/java/cn/chenghuan/wechatorder/controller/OrderController.java
@@ -1,15 +1,21 @@
 package cn.chenghuan.wechatorder.controller;
 
+import cn.chenghuan.wechatorder.domain.OrderDetail;
+import cn.chenghuan.wechatorder.dto.OrderDTO;
 import cn.chenghuan.wechatorder.enums.ExceptionEnum;
 import cn.chenghuan.wechatorder.exception.EmptyValueException;
+import cn.chenghuan.wechatorder.service.IOrderService;
 import cn.chenghuan.wechatorder.vo.OrderVO;
 import cn.chenghuan.wechatorder.vo.ResultVO;
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,6 +27,12 @@ import java.util.Map;
 @RequestMapping("/buyer/order/create")
 public class OrderController {
 
+    /**
+     * 订单service
+     */
+    @Autowired
+    private IOrderService orderService;
+
 
     /**
      * 创建订单
@@ -29,11 +41,30 @@ public class OrderController {
      * @return ResultVO<Map<String,String>>
      */
     @PostMapping
-    public ResultVO<Map<String,String>> create(@Valid final OrderVO orderVO, final BindingResult bindingResult){
+    public ResultVO<Map<String,String>> create(@Valid @RequestBody final OrderVO orderVO,
+                                               final BindingResult bindingResult){
         if (bindingResult.hasErrors()) {
            throw new EmptyValueException(ExceptionEnum.EMPTY_VALUE.getCode(),
                    bindingResult.getFieldError().getDefaultMessage());
         }
+        final OrderDTO orderDTO = orderVOTransferOrderDTO(orderVO);
+        orderService.createOrder(orderDTO);
         return null;
+    }
+
+    /**
+     * orderVO转orderDTO
+     * @param orderVO
+     * @return OrderDTO
+     */
+    private OrderDTO orderVOTransferOrderDTO(final OrderVO orderVO){
+        final OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setBuyerName(orderVO.getName());
+        orderDTO.setBuyerPhone(orderVO.getPhone());
+        orderDTO.setBuyerOpenid(orderVO.getOpenid());
+        orderDTO.setBuyerAddress(orderVO.getAddress());
+        final List<OrderDetail> orderDetailList = JSON.parseArray(orderVO.getItems(),OrderDetail.class);
+        orderDTO.setOrderDetailList(orderDetailList);
+        return orderDTO;
     }
 }
