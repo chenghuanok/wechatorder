b6f2a37afe6996bce86ce2eb877afc3e62b968be
chenghuanok <1832398432@qq.com>
2019-07-31 22:23:33 +0800

    订单支付controller

--- src/main/java/cn/chenghuan/wechatorder/controller/PayController.java
+++ src/main/java/cn/chenghuan/wechatorder/controller/PayController.java
@@ -1,10 +1,17 @@
 package cn.chenghuan.wechatorder.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author 程欢
  * @Description 支付controller
  * @Date 2019/7/16 23:04
  */
+@RestController
+@RequestMapping("/pay/order")
+@Slf4j
 public class PayController {
 
 }
